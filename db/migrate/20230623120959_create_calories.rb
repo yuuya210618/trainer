e761a576cc21255982c6775a00c805b614c4acf2
yuuya210618 <yuuya210618@gmail.com>
2023-06-27 08:49:19 +0900

    viewstash解決

---
+++ db/migrate/20230623120959_create_calories.rb
@@ -0,0 +1,9 @@
+class CreateCalories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :calories do |t|
+      t.references :user,               null: false, foreign_key: true
+      t.integer    :calories_intake,    null: false
+      t.timestamps
+    end
+  end
+end
