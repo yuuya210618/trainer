8c7c829fae82d305d6a1286efb7d77dd99fd09aa
yuuya210618 <yuuya210618@gmail.com>
2023-06-26 21:05:10 +0900

    削除機能についてコントローラー修正

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,6 +1,5 @@
 class CaloriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show]
 
   def index
     @calories = Calory.all
