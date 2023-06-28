b0959411ae5f8485dee811b54fee12dfff2c9aba
yuuya210618 <yuuya210618@gmail.com>
2023-06-28 18:25:08 +0900

    不要マイグレーションファイル削除

--- db/migrate/20230623120959_create_calories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCalories < ActiveRecord::Migration[6.0]
-  def change
-    create_table :calories do |t|
-      t.references :user,               null: false, foreign_key: true
-      t.integer    :calories_intake,    null: false
-      t.timestamps
-    end
-  end
-end
